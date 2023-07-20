81939c41581ce3109ca9c40986c097b6243811c0
Jacob <theonlyjacfresh@aol.com>
2023-07-19 20:05:04 -0400

    finish some levels

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,17 @@
  * DOTS: Level One
  *
  */
+// console.log("hello")
+const scoreboard = document.querySelector (".score")
+const ball = document.querySelector (".ball")
+const winner = document.querySelector(".level-winner")
+let score = 0
+ball.addEventListener ("click",()=>{
+    score += 10
+    scoreboard.textContent = score
+    if (score>=100){
+        winner.style.opacity = 1
+    }
+    console.log("ball was clicked")} )
+
+
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,19 @@
  * DOTS: Level Two
  *
  */
+const scoreboard = document.querySelector (".score")
+const arena = document.querySelector (".level-arena")
+const winner = document.querySelector(".level-winner")
+let score = 0
+arena.addEventListener ("click",(event)=>{
+    const el = event.target
+    if (el.classList.contains("ball")){
+
+        score += 10
+        scoreboard.textContent = score
+        if (score>=100){
+            winner.style.opacity = 1
+        }
+        console.log("ball was clicked")
+    }
+} )
